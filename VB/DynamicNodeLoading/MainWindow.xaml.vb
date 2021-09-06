339f589afce26619df8a703d673881fcbf2dbe4f
DevExpressExampleBot <fakeEmail>
2021-09-06 21:04:10 +0300

    Source auto update [skip ci]

--- VB/DynamicNodeLoading/MainWindow.xaml.vb
+++ VB/DynamicNodeLoading/MainWindow.xaml.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Windows
 Imports DevExpress.Xpf.Grid
 Imports System.IO
@@ -11,6 +10,7 @@ Namespace DynamicNodeLoading
 	''' </summary>
 	Partial Public Class MainWindow
 		Inherits Window
+
 		Public Sub New()
 			InitializeComponent()
 			Helper = New FileSystemHelper()
@@ -25,15 +25,7 @@ Namespace DynamicNodeLoading
 			End If
 		End Sub
 
-		Private privateHelper As FileSystemDataProvider
 		Private Property Helper() As FileSystemDataProvider
-			Get
-				Return privateHelper
-			End Get
-			Set(ByVal value As FileSystemDataProvider)
-				privateHelper = value
-			End Set
-		End Property
 
 		Public Sub InitDrives()
 			grid.BeginDataUpdate()
@@ -129,61 +121,29 @@ Namespace DynamicNodeLoading
 			End Function
 
 			Public Overrides Function GetDirectoryName(ByVal path As String) As String
-				Return New DirectoryInfo(path).Name
+				Return (New DirectoryInfo(path)).Name
 			End Function
 
 			Public Overrides Function GetFileName(ByVal path As String) As String
-				Return New FileInfo(path).Name
+				Return (New FileInfo(path)).Name
 			End Function
 
 			Public Overrides Function GetFileSize(ByVal path As String) As Long
-				Return New FileInfo(path).Length
+				Return (New FileInfo(path)).Length
 			End Function
 		End Class
 	End Class
 
 	Public Class FileSystemItem
-		Public Sub New(ByVal item_name As String, ByVal item_type As String, ByVal item_size As String, ByVal item_fullName As String)
-			Name = item_name
-			ItemType = item_type
-			Size = item_size
-			FullName = item_fullName
+		Public Sub New(ByVal name As String, ByVal type As String, ByVal size As String, ByVal fullName As String)
+			Me.Name = name
+			ItemType = type
+			Me.Size = size
+			Me.FullName = fullName
 		End Sub
-		Private privateName As String
 		Public Property Name() As String
-			Get
-				Return privateName
-			End Get
-			Set(ByVal value As String)
-				privateName = value
-			End Set
-		End Property
-		Private privateItemType As String
 		Public Property ItemType() As String
-			Get
-				Return privateItemType
-			End Get
-			Set(ByVal value As String)
-				privateItemType = value
-			End Set
-		End Property
-		Private privateSize As String
 		Public Property Size() As String
-			Get
-				Return privateSize
-			End Get
-			Set(ByVal value As String)
-				privateSize = value
-			End Set
-		End Property
-		Private privateFullName As String
 		Public Property FullName() As String
-			Get
-				Return privateFullName
-			End Get
-			Set(ByVal value As String)
-				privateFullName = value
-			End Set
-		End Property
 	End Class
 End Namespace
