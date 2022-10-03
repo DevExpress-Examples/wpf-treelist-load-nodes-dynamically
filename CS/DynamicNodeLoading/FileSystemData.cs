3131de6baaef5ccc129323e6d34b5b6f669eac0d
AndreySlabov <andrey.slabov@devexpress.com>
2022-10-03 16:31:07 +0400

    Update Sample

---
+++ CS/DynamicNodeLoading/FileSystemData.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DynamicNodeLoading {
+    public class FileSystemItem {
+        public FileSystemItem(string name, string type, string size, string fullName) {
+            Name = name;
+            ItemType = type;
+            Size = size;
+            FullName = fullName;
+        }
+        public string Name { get; set; }
+        public string ItemType { get; set; }
+        public string Size { get; set; }
+        public string FullName { get; set; }
+    }
+
+    public abstract class FileSystemDataProvider {
+        public abstract string[] GetLogicalDrives();
+        public abstract string[] GetDirectories(string path);
+        public abstract string[] GetFiles(string path);
+        public abstract string GetDirectoryName(string path);
+        public abstract string GetFileName(string path);
+        public abstract string GetFileSize(string path);
+        internal string GetFileSize(long size) {
+            if (size >= 1024)
+                return string.Format("{0:### ### ###} KB", size / 1024);
+            return string.Format("{0} Bytes", size);
+        }
+    }
+
+    public class FileSystemHelper : FileSystemDataProvider {
+        public override string[] GetLogicalDrives() {
+            return Directory.GetLogicalDrives();
+        }
+
+        public override string[] GetDirectories(string path) {
+            try {
+                return Directory.EnumerateDirectories(path).ToArray();
+            }
+            catch (UnauthorizedAccessException) {
+                return new string[] { };
+            }
+        }
+
+        public override string[] GetFiles(string path) {
+            try {
+                return Directory.EnumerateFiles(path).ToArray();
+            }
+            catch (UnauthorizedAccessException) {
+                return new string[] { };
+            }
+        }
+
+        public override string GetDirectoryName(string path) {
+            return new DirectoryInfo(path).Name;
+        }
+
+        public override string GetFileName(string path) {
+            return new FileInfo(path).Name;
+        }
+
+        public override string GetFileSize(string path) {
+            long size = new FileInfo(path).Length;
+            return GetFileSize(size);
+        }
+    }
+}
--- CS/DynamicNodeLoading/MainWindow.xaml.cs
+++ CS/DynamicNodeLoading/MainWindow.xaml.cs
@@ -1,30 +1,15 @@
-﻿using System;
-using System.Windows;
+﻿using DevExpress.Utils;
 using DevExpress.Xpf.Grid;
-using System.IO;
-using DevExpress.Utils;
+using System.Windows;
 
 namespace DynamicNodeLoading {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window {
+        FileSystemDataProvider Helper { get; set; }
         public MainWindow() {
             InitializeComponent();
             Helper = new FileSystemHelper();
             InitDrives();
         }
-
-        private void treeListView1_NodeExpanding(object sender, DevExpress.Xpf.Grid.TreeList.TreeListNodeAllowEventArgs e) {
-            TreeListNode node = e.Node;
-            if (node.Tag == null || (bool)node.Tag == false) {
-                InitFolder(node);
-                node.Tag = true;
-            }
-        }
-
-        FileSystemDataProvider Helper { get; set; }
-
         public void InitDrives() {
             grid.BeginDataUpdate();
             try {
@@ -32,33 +17,27 @@ namespace DynamicNodeLoading {
 
                 foreach (string s in root) {
                     TreeListNode node = new TreeListNode() { Content = new FileSystemItem(s, "Drive", "<Drive>", s) };
-                    treeListView1.Nodes.Add(node);
+                    view.Nodes.Add(node);
                     node.IsExpandButtonVisible = DefaultBoolean.True;
                 }
             }
             catch { }
             grid.EndDataUpdate();
         }
+
+        private void OnNodeExpanding(object sender, DevExpress.Xpf.Grid.TreeList.TreeListNodeAllowEventArgs e) {
+            TreeListNode node = e.Node;
+            if (node.Tag == null || (bool)node.Tag == false) {
+                InitFolder(node);
+                node.Tag = true;
+            }
+        }
+
         private void InitFolder(TreeListNode treeListNode) {
             grid.BeginDataUpdate();
             InitFolders(treeListNode);
             InitFiles(treeListNode);
             grid.EndDataUpdate();
-        }
-
-        private void InitFiles(TreeListNode treeListNode) {
-            FileSystemItem item = treeListNode.Content as FileSystemItem;
-            if (item == null) return;
-            TreeListNode node;
-            try {
-                string[] root = Helper.GetFiles(item.FullName);
-                foreach (string s in root) {
-                    node = new TreeListNode() { Content = new FileSystemItem(Helper.GetFileName(s), "File", Helper.GetFileSize(s).ToString(), s) };
-                    node.IsExpandButtonVisible = DefaultBoolean.False;
-                    treeListNode.Nodes.Add(node);
-                }
-            }
-            catch { }
         }
 
         private void InitFolders(TreeListNode treeListNode) {
@@ -80,6 +59,21 @@ namespace DynamicNodeLoading {
             catch { }
         }
 
+        private void InitFiles(TreeListNode treeListNode) {
+            FileSystemItem item = treeListNode.Content as FileSystemItem;
+            if (item == null) return;
+            TreeListNode node;
+            try {
+                string[] root = Helper.GetFiles(item.FullName);
+                foreach (string s in root) {
+                    node = new TreeListNode() { Content = new FileSystemItem(Helper.GetFileName(s), "File", Helper.GetFileSize(s).ToString(), s) };
+                    node.IsExpandButtonVisible = DefaultBoolean.False;
+                    treeListNode.Nodes.Add(node);
+                }
+            }
+            catch { }
+        }
+
         private bool HasFiles(string path) {
             string[] root = Helper.GetFiles(path);
             if (root.Length > 0) return true;
@@ -87,52 +81,5 @@ namespace DynamicNodeLoading {
             if (root.Length > 0) return true;
             return false;
         }
-        public abstract class FileSystemDataProvider {
-            public abstract string[] GetLogicalDrives();
-            public abstract string[] GetDirectories(string path);
-            public abstract string[] GetFiles(string path);
-            public abstract string GetDirectoryName(string path);
-            public abstract string GetFileName(string path);
-            public abstract long GetFileSize(string path);
-        }
-        public class FileSystemHelper : FileSystemDataProvider {
-
-            public override string[] GetLogicalDrives() {
-                return Directory.GetLogicalDrives();
-            }
-
-            public override string[] GetDirectories(string path) {
-                return Directory.GetDirectories(path);
-            }
-
-            public override string[] GetFiles(string path) {
-                return Directory.GetFiles(path);
-            }
-
-            public override string GetDirectoryName(string path) {
-                return new DirectoryInfo(path).Name;
-            }
-
-            public override string GetFileName(string path) {
-                return new FileInfo(path).Name;
-            }
-
-            public override long GetFileSize(string path) {
-                return new FileInfo(path).Length;
-            }
-        }
-    }
-
-    public class FileSystemItem {
-        public FileSystemItem(string name, string type, string size, string fullName) {
-            Name = name;
-            ItemType = type;
-            Size = size;
-            FullName = fullName;
-        }
-        public string Name { get; set; }
-        public string ItemType { get; set; }
-        public string Size { get; set; }
-        public string FullName { get; set; }
     }
 }
